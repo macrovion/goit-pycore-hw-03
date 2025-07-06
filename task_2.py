f47b2db91901a432020bbfa4d4b883b933678869
macrovion <macrovion@gmail.com>
2025-07-07 00:57:24 +0300

    Update task_2.py

--- task_2.py
+++ task_2.py
@@ -1,1 +1,18 @@
+import random
 
+def get_numbers_ticket(min_val, max_val, quantity):
+        
+    # Перевірка валідності вхідних параметрів
+    if (min_val < 1 or 
+        max_val > 1000 or 
+        min_val > max_val or 
+        quantity < 1 or 
+        quantity > (max_val - min_val + 1)):
+        return []
+    
+    # Генерація унікальних чисел
+    numbers = random.sample(range(min_val, max_val + 1), quantity)
+    
+    # Повертаємо відсортований список
+    return sorted(numbers)
+  
