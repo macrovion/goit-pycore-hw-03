c74eae054ce0873daa24823b9da95ed9c78ed353
macrovion <macrovion@gmail.com>
2025-07-07 01:05:43 +0300

    Update task_3.py

--- task_3.py
+++ task_3.py
@@ -1,1 +1,30 @@
+import re
+
+def normalize_phone(phone_number):
+    
+    # Видаляємо всі символи, крім цифр та '+'
+    cleaned = re.sub(r'[^\d+]', '', phone_number)
+    
+    # Якщо номер починається з '+380', залишаємо як є
+    if cleaned.startswith('+380'):
+        return cleaned
+    
+    # Якщо номер починається з '+38', але не з '+380', додаємо '0'
+    if cleaned.startswith('+38'):
+        return '+380' + cleaned[3:]
+    
+    # Якщо номер починається з '380', додаємо '+'
+    if cleaned.startswith('380'):
+        return '+' + cleaned
+    
+    # Якщо номер починається з '38', додаємо '+' та '0'
+    if cleaned.startswith('38'):
+        return '+380' + cleaned[2:]
+    
+    # Якщо номер починається з '0', замінюємо на '+380'
+    if cleaned.startswith('0'):
+        return '+38' + cleaned
+    
+    # Якщо номер не має префікса, додаємо '+380'
+    return '+380' + cleaned
 
